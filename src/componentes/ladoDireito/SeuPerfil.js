8b1d19734baa4d58531b753974d7d18cf8f9d341
Arthur Selingin <arthur_santos@outlook.com.br>
2024-04-16 22:00:24 -0300

    começando perfil

--- src/componentes/ladoDireito/SeuPerfil.js
+++ src/componentes/ladoDireito/SeuPerfil.js
@@ -1,14 +1,14 @@
 
-
+import foto from '../../imagens/images.png'
 import './SeuPerfil.css'
 function SeuPerfil() {
   return (
     <div className='SeuPerfil'>
         <div className='containerImg'>
-
+            <img src={foto}/>
         </div>
         <div className='containerNome'>
-    
+
         </div>
         <div className='containerButton'>
 

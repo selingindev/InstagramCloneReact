31549b606872ca8e2e8e0af0527b5f8987386730
Arthur Selingin <arthur_santos@outlook.com.br>
2024-04-19 19:35:12 -0300

    fazendo post

--- src/componentes/centro/ConteudoCentral.js
+++ src/componentes/centro/ConteudoCentral.js
@@ -3,9 +3,10 @@ import Post from "./Post"
 import './ConteudoCentral.css'
 import Storie1 from '../../imagens/fallenPerfil.jpeg'
 import Storie2 from '../../imagens/perfilCorinthians.jpg'
-import Storie3 from '../../imagens//perfilJojo.png'
-
-
+import Storie3 from '../../imagens/perfilJojo.png'
+import Storie4 from "../../imagens/Captura de tela_20230404_004053.png"
+import Storie5 from '../../imagens/chorao.JPG'
+import Storie6 from '../../imagens/Captura de tela_20230705_203658.png'
 function ConteudoCentral () {
   return (
     <div className="conteudoCentral">
@@ -17,13 +18,12 @@ function ConteudoCentral () {
             nomeStorie = 'SCCP'/>
             <Stories fotoStorie = {Storie3}
             nomeStorie = 'JojoWorld'/>
-            <Stories fotoStorie = 'art'
-            nomeStorie = 'SelinginSk8'/>
-             <Stories fotoStorie = 'art'
-            nomeStorie = 'SelinginSk8'/>
-            <Stories fotoStorie = 'art'
-            nomeStorie = 'SelinginSk8'/>
-            
+            <Stories fotoStorie = {Storie4}
+            nomeStorie = 'FUNKiROCK'/>
+             <Stories fotoStorie = {Storie5}
+            nomeStorie = 'Chorãozinho'/>
+            <Stories fotoStorie = {Storie6}
+            nomeStorie = 'SCPBoyz'/>    
         </div>
         <div className="containerPost">   
             <Post/>
--- src/componentes/centro/Post.js
+++ src/componentes/centro/Post.js
@@ -1,17 +1,61 @@
-
 export default function Post(props) {
   return (
       <div className="cardPost">
           <div className="cardHeader">
-              <h3>{props.title}</h3>
+            <div className="leftHeaderFoto"> 
+                <div className="containerFoto">
+                    <img src={props.fotoUser} alt="Foto do usúario na postagem"/>
+                </div>      
+                <div className="containerUser">
+                    <h3>{props.title}</h3>
+                </div>
+             </div>
+             <div className="rightHeaderFoto">
+
+             </div>
           </div>
           <div className="cardBody">
-              <img src={props.imgSrc} />
-              <p>{props.body}</p>
+            <div className="containerFoto">
+              <img src={props.imgPostagem} />
+            </div>
+            <div className="interacoesPostagem">
+                <div className="containerButtons">
+                    <div className="leftButtons">
+
+                    </div>
+                    <div className="rightButtons">
+
+                    </div>
+                </div>
+                <div className="curtidoInfo">
+                    <p>{}</p>
+                </div>
+        
+            </div>
           </div>
           <div className="card-footer">
-              
+            <div className="descPostagem">
+                <h4>{}</h4>
+                <p>{}</p>
+            </div>
+            <div className="comentarios">
+                <div className="todosComentarios">
+                    <p>{}</p>
+                </div>
+                <div className="seuComentario">
+                    <div className="comentarioFoto">
+                        <img src=''></img>
+                    </div>
+                    <div className="digiteAqui">
+                        <p></p>
+                    </div>
+                <div/>
+                <div className="horarioPost">
+                    <p></p>
+                </div>
+            </div>
           </div>
       </div>
+    </div>
   )
 }
